f80d1cdf12f786e2e1f4616bc7b593d56b142368
RonHerrmann <84956778+RonHerrmann@users.noreply.github.com>
2022-06-09 16:39:33 +0200

    Update lab04.c

--- Lab04_Wave.X/Lab04_Wave.X/src/lab04.c
+++ Lab04_Wave.X/Lab04_Wave.X/src/lab04.c
@@ -14,10 +14,12 @@
 
 // signal parameter
 
-#define sineWaveFrequency 0x00;
-#define sampleRate 0x00;
-#define minimalVoltage 0x00;
-#define maximalVoltage 0x00;
+#define sineWaveFrequency_Hz 1
+#define sampleRate_s 1
+#define minimalVoltage 1
+#define maximalVoltage -1
+
+#define getPR3(time) ((12.8e6 * time) / 256)
 
 
 /*
@@ -30,17 +32,28 @@
 #define TCKPS_256 0x03
 
 void timer_initialize()  // Timer 3
-{
+{   
+      
    T3CONbits.TON = 0; // Disable the Timers
-   T3CONbits.TCKPS = TCKPS_1; // Set Prescaler 1:1
+   T3CONbits.TCKPS = TCKPS_256; // Set Prescaler 1:1
    T3CONbits.TCS = 0; // Set Clock Source (internal = 0)
    T3CONbits.TGATE = 0; // Set Gated Timer Mode -> don't use gating //this line can be ignored, if TCS =  1 (have a look at the manual)  
-   PR3 = 10; // Load Timer Periods (highets value possible?)
+   PR3 = getPR3(sampleRate_s); // Load Timer Periods 
    TMR3 = 0x00; // Reset Timer Values
    IPC2bits.T3IP = 0x01; // Set Interrupt Priority (actually Level 1)
    CLEARBIT(IFS0bits.T3IF); //= 0; // Clear Interrupt Flags
-   IEC0bits.T3IE = 0; // Enable Interrupts --> (important: all)
+   IEC0bits.T3IE = 1; // Enable Interrupts --> (important: all)
    T3CONbits.TON = 1; // Enable the Timers  
+}
+
+// interrupt service routine
+
+void __attribute__((__interrupt__, __shadow__, __auto_psv__)) _T3Interrupt(void)
+{   
+    
+    TOGGLELED(LED1_PORT);
+    
+    IFS0bits.T3IF = 0;  
 }
 
 /*
@@ -52,8 +65,18 @@ void main_loop()
     // print assignment information
     lcd_printf("Lab04: Wave");
     lcd_locate(0, 1);
-    lcd_printf("Group: GroupName");
+    lcd_printf("Group: Boyang & Ron");
     
-    while(TRUE) { }
+    CLEARBIT(LED1_TRIS);   //set LED1 as output
+    
+    while(TRUE) { 
+        
+    uint16_t milliVolt = 0;    
+    
+    // Vout = Vamplitude * sin(omega_signal * t) + Voffset, with omega_signal = 2 * pi * f_signal
+        
+    dac_convert_milli_volt(milliVolt);  // set output voltage to DAC
+    
+    }
 }
 
