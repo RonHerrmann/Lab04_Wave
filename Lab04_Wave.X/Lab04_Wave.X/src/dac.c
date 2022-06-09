98fb348c44ff07470b891b53e0dfd5ca1198fd88
RonHerrmann <84956778+RonHerrmann@users.noreply.github.com>
2022-06-09 16:40:06 +0200

    Update dac.c

--- Lab04_Wave.X/Lab04_Wave.X/src/dac.c
+++ Lab04_Wave.X/Lab04_Wave.X/src/dac.c
@@ -1,4 +1,7 @@
 #include "dac.h"
+#include "types.h"
+
+
 
 // tristate register
 #define DAC_CS_TRIS TRISDbits.TRISD8
@@ -24,19 +27,7 @@
 
 void dac_initialize()
 {
-    // set AN10, AN11 AN13 to digital mode
-    // this means AN10 will become RB10, AN11->RB11, AN13->RB13
-    // see datasheet 11.3
-    
-    // set RD8, RB10, RB11, RB13 as output pins
-    
-    // set default state: CS=on, SCK=off, SDI=off, LDAC=on
-    
-    
-    
-    *************************
-    /*
-     ETBIT(DAC_SDI_AD1CFG);
+    SETBIT(DAC_SDI_AD1CFG);
     SETBIT(DAC_SDI_AD2CFG);  // set AN10 to digital mode
     
     SETBIT(DAC_SCK_AD1CFG);
@@ -48,21 +39,37 @@ void dac_initialize()
     // this means AN10 will become RB10, AN11->RB11, AN13->RB13
     // see datasheet 11.3
     
+    
     CLEARBIT(DAC_CS_TRIS);
     CLEARBIT(DAC_SDI_TRIS);
     CLEARBIT(DAC_SCK_TRIS); 
     CLEARBIT(DAC_LDAC_TRIS);    // set RD8, RB10, RB11, RB13 as output pins
     
-    SETBIT(DAC_CS_PORT); // Low to enable 
-    CLEARBIT(DAC_SCK_PORT); // wirte on rising edge
+    // set default state: CS=on, SCK=off, SDI=off, LDAC=on
+     
+    SETBIT(DAC_CS_PORT); // high to enable 
+    CLEARBIT(DAC_SCK_PORT); // write on rising edge
     CLEARBIT(DAC_SDI_PORT); // Bit to send Data to DAC
     SETBIT(DAC_LDAC_PORT);  // set default state: CS=1, SCK=0, SDI=undefined, LDAC=1 (Datasheet MCP4822 Page 23)
-     */
-    ****************************
-
+   
 }
+
 
 void dac_convert_milli_volt(uint16_t milliVolt)
 {
+    CLEARBIT(DAC_CS_PORT);  // Set CS bit to zero to start conversation
     
+    uint8_t var = 16;
+    
+    for(var; var >= 1; var--){      // Set Binary-Code for Output signal by setting SDI Bit high or low
+        
+        Nop();
+        CLEARBIT(DAC_SCK_PORT);
+        Nop();
+        
+        DAC_SDI_PORT = milliVolt >> (var -1) & BV(0);
+        
+        Nop();
+        SETBIT(DAC_SCK_PORT);  
+    }
 }
