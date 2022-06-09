9c768db7cd49a00af32927d0ca0777919407662b
RonHerrmann <84956778+RonHerrmann@users.noreply.github.com>
2022-06-09 14:24:17 +0200

    First implementation

--- Lab04_Wave.X/Lab04_Wave.X/src/lab04.c
+++ Lab04_Wave.X/Lab04_Wave.X/src/lab04.c
@@ -14,20 +14,33 @@
 
 // signal parameter
 
+#define sineWaveFrequency 0x00;
+#define sampleRate 0x00;
+#define minimalVoltage 0x00;
+#define maximalVoltage 0x00;
 
 
 /*
  * Timer code
  */
-
+    
 #define TCKPS_1   0x00
 #define TCKPS_8   0x01
 #define TCKPS_64  0x02
 #define TCKPS_256 0x03
 
-void timer_initialize()
+void timer_initialize()  // Timer 3
 {
-    
+   T3CONbits.TON = 0; // Disable the Timers
+   T3CONbits.TCKPS = TCKPS_1; // Set Prescaler 1:1
+   T3CONbits.TCS = 0; // Set Clock Source (internal = 0)
+   T3CONbits.TGATE = 0; // Set Gated Timer Mode -> don't use gating //this line can be ignored, if TCS =  1 (have a look at the manual)  
+   PR3 = 10; // Load Timer Periods (highets value possible?)
+   TMR3 = 0x00; // Reset Timer Values
+   IPC2bits.T3IP = 0x01; // Set Interrupt Priority (actually Level 1)
+   CLEARBIT(IFS0bits.T3IF); //= 0; // Clear Interrupt Flags
+   IEC0bits.T3IE = 0; // Enable Interrupts --> (important: all)
+   T3CONbits.TON = 1; // Enable the Timers  
 }
 
 /*
@@ -43,3 +56,4 @@ void main_loop()
     
     while(TRUE) { }
 }
+
